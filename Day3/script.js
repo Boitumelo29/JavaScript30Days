07592ce5e76996e1cbec1fc475b90878b66fe305
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-15 13:50:01 +0200

    update

--- Day3/script.js
+++ Day3/script.js
@@ -1,9 +1,15 @@
 function clock() {
-    let seconds = document.getElementById('seconds ');
+    let seconds = document.getElementById('seconds');
+    let minutes = document.getElementById('minutes');
+    let hours = document.getElementById('hour');
 
     let s = new Date().getSeconds();
-
+    let m = new Date().getMinutes();
+    let h = new Date().getHours();
     seconds.innerHTML = s;
+    minutes.innerHTML = m;
+    hours.innerHTML = h;
+
 
 };
 
