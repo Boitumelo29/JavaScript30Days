9f4cd2898ef6ec0ddf34dc98b54db38004551fe0
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-16 14:42:49 +0200

    update

--- Day8/script.js
+++ Day8/script.js
@@ -1,12 +1,13 @@
-function shuffle() {
-    const img = document.getElementById('img')
-    let random = Math.floor(Math.random() * 6) + 1;
-    img.setAttribute('src', `/Day7/images/${random}.png`);
+const emojis = ["😆", "😅", "🤣", "😂", "😀", "🤑", "🤨", "🙂", "😊", "😗", "😛", "😏", "🤥", "😴", "🥺", "😧", "🤗", "🤩",
+    "😎", "🥳", "😍", "😱", "🤓", "😷", "🥴", "😳", "🤯", "🤫", "🤑", "😪", "😴", "😵", "😶‍🌫️", "🥵", "🥶", "😳", "🤪", "😵", "🥴",
+    "😵‍💫", "😠", "😡", "🤮", "🤢", "🤒", "😷", "🤬", "🤧", "😇", "🥳"];
 
-}
+const btn = document.getElementById('emoji-btn');
 
-function animation() {
-    setTimeout(shuffle, 500);
-    const img = document.getElementById('img')
-    img.setAttribute('src', `/Day7/images/dice-rol.gif`);
-}
+btn.addEventListener('mouseover', function (params) {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+})
+
+btn.addEventListener('click', function (params) {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+})
