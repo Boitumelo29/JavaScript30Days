6c0ce78f44a19a75bd3b2533f18ef04e7aeac528
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-19 18:56:43 +0200

    update

--- Day21/script.js
+++ Day21/script.js
@@ -1,7 +1,20 @@
-const output = document.getElementById('output');
+const wrapper = document.querySelector('.wrapper');
 
-window.addEventListener('mousemove', (e) => {
-    let xpos = e.clientX;
-    let ypos = e.clientY;
-    output.innerHTML = `<div><span>X:</span> ${xpos}</div> <div><span>Y:</span> ${ypos}</div>`
-})
+function onDrag({ movementX, movementY }) {
+    let getStyle = window.getComputedStyle(wrapper);
+    let left = parseInt(getStyle.left);
+    let top = parseInt(getStyle.top);
+    wrapper.style.left = `${left + movementX}px`
+    wrapper.style.top = `${top + movementY}px`
+}
+
+wrapper.addEventListener('mousedown', () => {
+    wrapper.classList.add('active');
+    wrapper.addEventListener('mousemove', onDrag);
+});
+
+
+wrapper.addEventListener('mouseup', () => {
+    wrapper.classList.remove('active');
+    wrapper.removeEventListener('mousemove', onDrag);
+});
