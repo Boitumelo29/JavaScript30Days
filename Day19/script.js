2dc6ae65b7037d4db90b425b7f1b33a8055d3e47
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-19 08:31:44 +0200

    update

--- Day19/script.js
+++ Day19/script.js
@@ -1,10 +1,3 @@
-const inputTextarea = document.getElementById('input-textare');
-const charCount = document.getElementById('charc-count');
-const wordCount = document.getElementById('word-count');
-
-inputTextarea.addEventListener('input', () => {
-    charCount.textContent = inputTextarea.value.length;
-
-    let txt = inputTextarea.value.trim();
-    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+window.addEventListener('keydown', (e) => {
+    document.getElementById('result').innerHTML = `The Key Press is <h2>${e.key}</h2> <p>The key code presses is: ${e.keyCode}</p>`
 })
