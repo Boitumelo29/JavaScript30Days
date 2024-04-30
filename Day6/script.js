f014db33c7128c3dcd5ddf7067eaac50d92def3c
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-30 09:53:21 +0200

    update on day06

--- Day6/script.js
+++ Day6/script.js
@@ -1,4 +1,20 @@
-const counter = document.getElementById('.counter');
-const reset = document.getElementById('.btn-reset');
-const increase = document.getElementById('.increase');
-const decrease = document.getElementById('.decrease');
+const counter = document.querySelector('.counter');
+const reset = document.querySelector('.btn-reset');
+const increase = document.querySelector('.increase');
+const decrease = document.querySelector('.decrease');
+
+let count = 0;
+
+increase.addEventListener('click', () => {
+    count++;
+    counter.innerHTML = count;
+})
+decrease.addEventListener('click', () => {
+    count--;
+    counter.innerHTML = count;
+})
+
+reset.addEventListener('click', () => {
+    count = 0;
+    counter.innerHTML = count;
+})
