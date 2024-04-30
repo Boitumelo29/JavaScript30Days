371c1c73375532281c32ba291c863c69cf2c2d6d
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-04-30 09:47:14 +0200

    java script update

--- Day6/script.js
+++ Day6/script.js
@@ -1,14 +1,4 @@
-let survery_option = document.getElementById('survey_options');
-let add_fields = document.getElementById('add_fields');
-let remove_fields = document.getElementById('remove_fields');
-
-
-add_fields.onclick = function () {
-    let newFields = document.createElement('input');
-    newFields.setAttribute('type', 'text');
-    newFields.setAttribute('Name', 'survey_options []');
-    newFields.setAttribute('class', 'survey_options []');
-    newFields.setAttribute('size', '50');
-    newFields.setAttribute('placeholder', 'Another Fields');
-    survery_option.appendChild(newFields);
-}
+const counter = document.getElementById('.counter');
+const reset = document.getElementById('.btn-reset');
+const increase = document.getElementById('.increase');
+const decrease = document.getElementById('.decrease');
