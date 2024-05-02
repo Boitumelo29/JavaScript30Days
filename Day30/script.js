4c49a02e83fedef78853baffe1878723056c604e
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-02 09:45:41 +0200

    day 30 update

--- Day30/script.js
+++ Day30/script.js
@@ -1,14 +1,16 @@
-let survery_option = document.getElementById('survey_options');
-let add_fields = document.getElementById('add_fields');
-let remove_fields = document.getElementById('remove_fields');
+const text = document.getElementById('heading');
 
 
-add_fields.onclick = function () {
-    let newFields = document.createElement('input');
-    newFields.setAttribute('type', 'text');
-    newFields.setAttribute('Name', 'survey_options []');
-    newFields.setAttribute('class', 'survey_options []');
-    newFields.setAttribute('size', '50');
-    newFields.setAttribute('placeholder', 'Another Fields');
-    survery_option.appendChild(newFields);
+const prog = 'We have finished our 30 days of javascript ';
+
+let idx = 1;
+
+setInterval(writeT, 150);
+
+function writeT() {
+    text.innerHTML = prog.slice(0, idx);
+    idx++
+    if (idx > prog.length) {
+        idx = 1;
+    }
 }
