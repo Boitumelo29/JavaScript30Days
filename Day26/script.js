bb427f4c669cd1f87ef9061c1f8a65c128ce1dd6
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-03 19:04:14 +0200

    javascript

--- Day26/script.js
+++ Day26/script.js
@@ -1,3 +1,7 @@
 const submit = document.getElementById('button');
 
 const outPut = document.getElementById('output');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const bullet = document.querySelectorAll('bullet');
+
