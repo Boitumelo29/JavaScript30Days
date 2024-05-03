ca269d7be8c9908240a20cd984a7231f803c5238
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-03 19:19:48 +0200

    update

--- Day26/script.js
+++ Day26/script.js
@@ -5,3 +5,26 @@ const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 const bullet = document.querySelectorAll('bullet');
 
+const MAX_STEPS = 4;
+let currentStep = 1;
+
+
+nextBtn.addEventListener('click', () => {
+    const currentBullet = bullet[currentStep - 1];
+    currentBullet.classList.add('completed');
+    currentBullet++;
+
+    prevBtn.disabled = false;
+
+    if (currentStep === MAX_STEPS) { nextBtn.disabled = true; }
+});
+
+prevBtn.addEventListener('click', () => {
+    const currentBullet = bullet[currentStep - 2];
+    currentBullet.classList.remove('completed');
+    currentBullet--;
+
+    nextBtn.disabled = false;
+
+    if (currentStep === 1) { prevBtn.disabled = true; }
+})
