9e73217dad9d8b0da137121d5c422a0f6c93a524
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-06 09:47:00 +0200

    update

--- Day22/script.js
+++ Day22/script.js
@@ -1,7 +1,34 @@
 let tags = [];
 
 function addTags(e) {
+    let code = (e.keyCode ? e.keyCode : e.which);
+    if (code != 13) {
+        return
+    }
 
+
+    let tag = e.target.value.trim();
+    if (tag.length < 1 || tags.includes(tag)) {
+        e.target.value = '';
+        return
+    }
+
+    let index = tags.push(tag);
+
+    let tagItem = document.createElement('div');
+    tagItem.classList.add('item');
+    tagItem.innerHTML = ` <span class="delete-btn" onclick ="deleteTag(this, ${tag})">&times;</span>
+    <span>${tag}</span>`;
+
+    document.querySelector('.tag-input .tag-list').appendChild(tagItem);
+    e.target.value = '';
 }
 
+
+function deleteTag(ref, tag) {
+    let parent = ref.parentnNode.parentNode;
+    parent.removeChild(ref.parenetNode);
+    let index = tags.indexOf(tag);
+    tag.splice(index);
+}
 document.querySelector('.tag-input input ').addEventListener('keyup', addTags)
