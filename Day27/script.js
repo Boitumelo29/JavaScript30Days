ad6da38093b48b7194754afde02677849d05ef05
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-07 08:45:53 +0200

    update

--- Day27/script.js
+++ Day27/script.js
@@ -2,3 +2,5 @@ async function getPost() {
     fetch(`https://rickandmortyapi.com/api/character/1`).then(function (response) {
         return response.json()
     }).then(function (data) {
+        addItemsCollection(data);
+    })
