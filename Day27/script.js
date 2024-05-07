2c92cbdc669e06bdec1f3a1f89c57945a1e31005
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-07 08:57:04 +0200

    update

--- Day27/script.js
+++ Day27/script.js
@@ -1,3 +1,5 @@
+getPost()
+
 async function getPost() {
     fetch(`https://rickandmortyapi.com/api/character/1`).then(function (response) {
         return response.json()
@@ -9,5 +11,10 @@ async function getPost() {
 function addItemsCollection(data) {
     const newElement = document.createElement('div')
     newElement.classList.add('item')
-    newElement.innerHTML = (``)
+    newElement.innerHTML = (`<div class="char-id">${data.id}</div>
+    <div class="char-name">${data.name}</div>
+    <img src="${data.image}" alt="" class="char-img">
+    <div class="char-species">${data.species}</div>`)
+
+    items_container.appendChild(newElement)
 }
