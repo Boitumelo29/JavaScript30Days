9e9f97bf829d5954dc9421a1aedd346bc5cc58ac
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-07 08:44:33 +0200

    update

--- Day27/script.js
+++ Day27/script.js
@@ -1,7 +1,1 @@
-const button = document.querySelector('button');
-
-button.addEventListener('click', function () {
-    let input = document.querySelector('input');
-    input.select();
-    document.execCommand('copy');
-})
+async function getPost() {
